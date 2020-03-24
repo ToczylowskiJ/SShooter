f7aa85bb8ff833da0f8bb432e70cfed13509fd8f
imzot <toczylowskija@gmail.com>
2020-03-24 14:05:23 -0400

    Made small changes to AI Map

--- Source/SShooter/PatrolRoute.h
+++ Source/SShooter/PatrolRoute.h
@@ -6,7 +6,9 @@
 #include "Components/ActorComponent.h"
 #include "PatrolRoute.generated.h"
 
-
+/**
+* A "route card" to help AI choose their next waypoint 
+*/
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SSHOOTER_API UPatrolRoute : public UActorComponent
 {
