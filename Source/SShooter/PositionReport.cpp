d208d7ad5872008ad70f763e2ed40bbf9cdbdfb5
David Aydin <da660655@sju.edu>
2020-03-25 22:12:06 -0400

    Binaries File Fix Up

--- Source/SShooter/PositionReport.cpp
+++ Source/SShooter/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "SShooter.h"
 #include "PositionReport.h"
+#include "SShooter.h"
 #include "Gameframework/Actor.h"
 
 
